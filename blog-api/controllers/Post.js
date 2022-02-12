e07c9c92b59c3ec011fda0de95c5c1a65e1319af
Ilya <senior.smikow@yandex.ru>
2022-02-12 15:01:04 +0300

    fix heroku 12

--- blog-api/controllers/Post.js
+++ blog-api/controllers/Post.js
@@ -56,10 +56,11 @@ module.exports.upload = async (req, res) => {
   }
 
   const { file } = req.files;
-  const uploadPath = [process.cwd(), '/public/uploads', file.md5 + '_' + file.name].join('/');
-  //const uploadPath = [process.cwd(), "/build", file.md5 + "_" + file.name].join(
-    "/"
-  );
+  const uploadPath = [
+    process.cwd(),
+    "/public/uploads",
+    file.md5 + "_" + file.name,
+  ].join("/");
 
   file.mv(uploadPath, function (err) {
     if (err) {
--- src/pages/Post/PostPage.js
+++ src/pages/Post/PostPage.js
@@ -46,10 +46,7 @@ export const PostPage = () => {
           <div className={styles.page__wrapper}>
             <div className={styles.page__header}>
               {postData && postData.photoUrl ? (
-                <img
-                  src={`react-my-blog.herokuapp.com/uploads/${postData.photoUrl}`}
-                  alt="cover"
-                />
+                <img src={`uploads/${postData.photoUrl}`} alt="cover" />
               ) : (
                 <img src={BackgroundImg} alt="cover" />
               )}
